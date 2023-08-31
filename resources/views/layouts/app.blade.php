027b6a445720d0a9f426cd0a73c1dd7230224e59
Marcola Garcia <akashwebroot@gmail.com>
2023-08-31 10:11:20 +0530

    Header Footer created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     @livewireStyles()
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
     @include('layouts.header')
     @yield('content')
     @include('layouts.footer')
     @livewireScripts()
 </body>
+
 </html>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,1 +1,93 @@
-<h2>Footer</h2>
+{{-- Footer Starts --}}
+
+<div class="mainFooter">
+    <div class="footerInner row justify-content-between">
+        <div class="col-md-4">
+            <h4>
+                Services
+            </h4>
+            <ul class="services">
+                <li><a href="#">Car valuation</a></li>
+                <li><a href="#">Scrap my car</a></li>
+                <li><a href="#">Sell my motorhome or campervan</a></li>
+            </ul>
+            <h4 class="tips">
+                Tips & guide
+            </h4>
+            <ul class="guides">
+                <li><a href="#">Valuation guides</a></li>
+                <li><a href="#">Selling & buying guides</a></li>
+                <li><a href="#">Car ownership guides</a></li>
+                <li><a href="#">Car maintenance guides</a></li>
+                <li><a href="#">Our blogs</a></li>
+            </ul>
+        </div>
+
+        <div class="col-md-4">
+            <h4>
+                Quick links
+            </h4>
+            <ul class="services">
+                <li><a href="#">Frequently asked questions</a></li>
+                <li><a href="#">Branch locator</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Just Sold My Car</a></li>
+                <li><a href="#">Unsubscribe</a></li>
+                <li><a href="#">Our affiliate programme</a></li>
+                <li><a href="#">Modern Slavery Act Statement</a></li>
+                <li><a href="#">Gender Pay Gap Report</a></li>
+                <li><a href="#">Tax Strategy</a></li>
+                <li><a href="#">Sitemap</a></li>
+            </ul>
+        </div>
+
+        <div class="col-md-4">
+            <h4>
+                Tools
+            </h4>
+            <ul class="services">
+                <li><a href="#">Free car check</a></li>
+                <li><a href="#">MOT check</a></li>
+                <li><a href="#">Car tax check</a></li>
+            </ul>
+            <h4 class="tips">
+                Partner sites
+            </h4>
+            <ul class="guides">
+                <li><a href="#">webuyanyvan.com</a></li>
+                <li><a href="#">cinch.co.uk</a></li>
+                <li><a href="#">webuyanycarusa.com</a></li>
+                <li><a href="#">Corporate Website</a></li>
+            </ul>
+
+            <h4 class="tips">
+                Social media
+            </h4>
+            <ul class="guides d-flex">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="copyrightMenu">
+        <div class="row">
+            <div class="col-md-6">
+                <ul class="d-flex">
+                    <li><a href="#">Terms & Conditions</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Cookie Policy</a></li>
+                    <li><a href="#">Cookie Preferences</a></li>
+                </ul>
+            </div>
+            <div class="col-md-6"">
+                <div class="copyright" style="float: right;">
+                    <p>
+                        © We Buy Any Car Ltd. 2006 - 2023</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
